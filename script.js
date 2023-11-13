389f80900e808c524f35202fce081da7a45b715f
João Victor <jvspepe@proton.me>
2023-11-13 16:07:52 -0300

    Add header styling.

---
+++ script.js
@@ -0,0 +1,31 @@
+const drawer = document.querySelector(".drawer");
+const drawerToggle = drawer.querySelector(".drawer-toggle");
+
+const drawerBackdrop = drawer.querySelector(".drawer-backdrop");
+const drawerContent = drawer.querySelector(".drawer-content");
+
+function handleDrawerToggle() {
+  const isOpen = drawerContent.dataset.open === "false";
+
+  // Prevent scrolling when open
+  document.documentElement.style.overflowY = isOpen ? "hidden" : "";
+
+  // Backdrop
+  drawerBackdrop.ariaHidden = !isOpen;
+  drawerBackdrop.dataset.open = isOpen;
+
+  // Content
+  drawerContent.ariaHidden = !isOpen;
+  drawerContent.dataset.open = isOpen;
+
+  // Toggle
+  drawerToggle.ariaExpanded = isOpen;
+  drawerToggle.ariaLabel = isOpen ? "Fechar Menu" : "Abrir Menu";
+  drawerToggle.title = isOpen ? "Fechar Menu" : "Abrir Menu";
+  drawerToggle.dataset.open = isOpen;
+  drawerToggle.innerHTML = `<svg aria-hidden="true" class="icon"><use class="drawer-toggle-icon" href="/icons/${
+    isOpen ? "x" : "menu"
+  }.svg#${isOpen ? "x" : "menu"}" /></svg>`;
+}
+
+drawerToggle.addEventListener("click", handleDrawerToggle);
